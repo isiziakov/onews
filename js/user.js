eedc940a77344d84f4fd3b203170bf056076f1cf
isiziakov <80616799+isiziakov@users.noreply.github.com>
2024-06-26 16:25:39 +0300

    Updated authorization

--- js/user.js
+++ js/user.js
@@ -4,8 +4,8 @@ function enterClick(){
     var email = document.getElementById('floatingInput').value;
     var password = document.getElementById('floatingPassword').value;
     var body = JSON.stringify({
-      email : "1",
-      password: "1"
+      email : email,
+      password: password
     });
 
     var xhr = new XMLHttpRequest();
@@ -14,6 +14,42 @@ function enterClick(){
     xhr.onload = function () {
       if (xhr.status == 401){
         document.getElementById ("error").style.visibility = "visible"
+      }
+    };
+    xhr.send(body);
+}
+
+function regClick(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var fio = document.getElementById('fio').value;
+    var phone = document.getElementById('phone').value;
+    var date = document.getElementById('date').value;
+    var sex = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
+    var role = document.getElementById('role').value;
+
+    var body = JSON.stringify({
+      email : email,
+      password: password,
+      birthdate: date,
+      sex: sex,
+      phone: phone,
+      name: fio,
+      role: role
+    });
+    
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url + "/Register", true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.onload = function () {
+      if (xhr.status == 401){
+        document.getElementById ("error").style.visibility = "visible"
+        document.getElementById ("error").innerHTML = xhr.responseText
+      }
+      if (xhr.status == 200){
+        document.getElementById ("error").style.visibility = "visible"
+        document.getElementById ("error").innerHTML = xhr.responseText
       }
     };
     xhr.send(body);
